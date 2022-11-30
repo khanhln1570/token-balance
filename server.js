c2eaca647d001d9716360b1314ccbacfc6eb0d81
khanhln <khanh.la@digitalunicorn.fr>
2022-11-30 15:13:45 +0700

    Update

--- server.js
+++ server.js
@@ -7,18 +7,7 @@ app.set("view engine", "ejs");
 
 // index page
 app.get("/", function (req, res) {
-  var mascots = [
-    { name: "Sammy", organization: "DigitalOcean", birth_year: 2012 },
-    { name: "Tux", organization: "Linux", birth_year: 1996 },
-    { name: "Moby Dock", organization: "Docker", birth_year: 2013 },
-  ];
-  var tagline =
-    "No programming concept is complete without a cute animal mascot.";
-
-  res.render("pages/index", {
-    mascots: mascots,
-    tagline: tagline,
-  });
+  res.render("pages/index");
 });
 
 // about page
