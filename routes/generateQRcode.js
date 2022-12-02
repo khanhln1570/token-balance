6678976ea257ef212844411b1c67f348a6686834
khanhln <khanh.la@digitalunicorn.fr>
2022-12-03 00:40:16 +0700

    Update QR code

--- routes/generateQRcode.js
+++ routes/generateQRcode.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const qr = require("qrcode");
 
 router.get("/", (req, res) => {
-  res.render("pages/generateQRcode",{image: null, url:null});
+  res.render("pages/generateQRcode",{image: null, url:null, message:null});
 });
 
 router.post("/generate", async function (req, res) {
   const url = req.body.url;
-  let qrcode = await qr.toDataURL(url);
-  return res.render('pages/generateQRcode',{image: qrcode, url: url})
+  if (url.toString().trim().startsWith(`https:`) || url.toString().trim().startsWith(`ipfs:`) ) {
+    let qrcode = await qr.toDataURL(url);
+    return res.render('pages/generateQRcode',{image: qrcode, url: url,message:null})
+  }
+  return res.render('pages/generateQRcode',{image: null, url: url, message: 'Wrong URL format!'})
 });
 module.exports = router;
