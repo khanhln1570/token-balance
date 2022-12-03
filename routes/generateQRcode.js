a1dba21c9eddd7b3f68860feaf018d9e6f326f6c
khanhln <khanh.la@digitalunicorn.fr>
2022-12-03 23:00:25 +0700

    Update QR code

--- routes/generateQRcode.js
+++ routes/generateQRcode.js
@@ -9,7 +9,12 @@ router.get("/", (req, res) => {
 router.post("/generate", async function (req, res) {
   const url = req.body.url;
   if (url.toString().trim().startsWith(`https:`) || url.toString().trim().startsWith(`ipfs:`) ) {
-    let qrcode = await qr.toDataURL(url);
+    let qrcode = await qr.toDataURL(url,{
+      width: 1024,
+      type: 'image/png',
+      margin: 2,
+      scale: 5
+    });
     return res.render('pages/generateQRcode',{image: qrcode, url: url,message:null})
   }
   return res.render('pages/generateQRcode',{image: null, url: url, message: 'Wrong URL format!'})
