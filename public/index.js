38fd0ee30d88f052598ce3cd87681dbb49026db8
khanhln <khanh.la@digitalunicorn.fr>
2022-12-29 11:53:45 +0700

    Update connect wallet

--- public/index.js
+++ public/index.js
@@ -22,7 +22,7 @@ async function apiCall() {
   function call(input) {
     if (input.length >= 1) {
       const APIKEY = "rBWqZx0eHdgw8DTBLKL0gDBVQz6FCm4C969AsgV15MvFB3Z4";
-     
+
       let url = `https://svc.blockdaemon.com/nft/v1/ethereum/mainnet/collections/search?apiKey=${APIKEY}&name=${input}`;
 
       fetch(url)
@@ -31,16 +31,21 @@ async function apiCall() {
           const result = data.data;
 
           collections = result.map((collection) => {
-            return { name: collection.name, logo: `https://svc.blockdaemon.com/nft/v1/ethereum/mainnet/media/${collection.logo}?apiKey=${APIKEY}` };
+            return {
+              name: collection.name,
+              logo: `https://svc.blockdaemon.com/nft/v1/ethereum/mainnet/media/${collection.logo}?apiKey=${APIKEY}`,
+            };
           });
           // autocomplete(document.getElementById("nameCollection"), collections);
           if (collections.length !== 0) {
-            autocomplete(document.getElementById("nameCollection"), collections);
+            autocomplete(
+              document.getElementById("nameCollection"),
+              collections
+            );
           }
         });
     }
   }
-  
 }
 
 function autocomplete(inp, arr) {
@@ -68,12 +73,12 @@ function autocomplete(inp, arr) {
     /*for each item in the array...*/
     for (i = 0; i < arr.length; i++) {
       // if (arr[i].replace(/\s/g,'').toLowerCase().includes(val.replace(/\s/g,'').toLowerCase()) ) {
-     
+
       /*create a DIV element for each matching element:*/
       b = document.createElement("DIV");
       /*make the matching letters bold:*/
-      
-      b.innerHTML = `<img style=" border-radius: 50%;width: 50px;height: 50px; margin-right: 20px" src=${arr[i].logo}>`
+
+      b.innerHTML = `<img style=" border-radius: 50%;width: 50px;height: 50px; margin-right: 20px" src=${arr[i].logo}>`;
       b.innerHTML += "<strong>" + arr[i].name + "</strong>";
       /*insert a input field that will hold the current array item's value:*/
       b.innerHTML += "<input type='hidden' value='" + arr[i].name + "'>";
@@ -147,3 +152,74 @@ function autocomplete(inp, arr) {
     closeAllLists(e.target);
   });
 }
+
+let account;
+
+// https://docs.walletconnect.com/quick-start/dapps/web3-provider
+let provider = new WalletConnectProvider.default({
+  rpc: {
+    1: "https://cloudflare-eth.com/", // https://ethereumnodes.com/
+    137: "https://polygon-rpc.com/", // https://docs.polygon.technology/docs/develop/network-details/network/
+    // ...
+  },
+  bridge: "https://bridge.walletconnect.org",
+});
+
+const connectWC = async () => {
+  window.mobileCheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+  };
+  if (window.mobileCheck()) {
+    try {
+      await provider.enable();
+  
+      //  Create Web3 instance
+      const web3 = new Web3(provider);
+      window.w3 = web3;
+  
+      var accounts = await web3.eth.getAccounts(); // get all connected accounts
+      account = accounts[0]; // get the primary account
+      document.getElementById("walletAddress").value = account;
+    } catch (error) {
+      console.warn(error);
+      location.reload();
+    }
+  } else {
+    ethereum.request({ method: 'eth_requestAccounts' }).then(accounts => {
+      account = accounts[0];
+      if (account) {
+        console.log(account);
+        document.getElementById("walletAddress").value = account;
+      } else {
+        alert("Fail")
+      }
+      ethereum.request({ method: 'eth_getBalance', params: [account, 'latest'] }).then(result => {
+        console.log(result);
+      });
+    });
+  }
+ 
+};
+
+const sign = async (msg) => {
+  if (w3) {
+    return await w3.eth.personal.sign(msg, account);
+  } else {
+    return false;
+  }
+};
+
+const contract = async (abi, address) => {
+  if (w3) {
+    return new w3.eth.Contract(abi, address);
+  } else {
+    return false;
+  }
+};
+
+const disconnect = async () => {
+  // Close provider session
+  await provider.disconnect();
+};
