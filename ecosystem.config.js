02955f7d9fe0fd1bb7be2645a43dd5404ce45e99
khanhln <khanh.la@digitalunicorn.fr>
2022-12-30 10:36:11 +0700

    Update

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [{
+        name: "nft-finder",
+        script: "npm",
+        args: "run start",
+        cwd: "/home/root/nftFinder/token-balance",
+        watch: ["server", "client"],
+        // Delay between restart
+        watch_delay: 1000,
+        ignore_watch: ["node_modules", "client/img"],
+        watch_options: {
+            "followSymlinks": false
+        },
+        env: {
+            "NODE_ENV": "development"
+        },
+    }]
+}
